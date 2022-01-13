d06101b92d9ad95350b0f12ed4e4974035a69ccc
Ashley <adunderwood96@gmail.com>
2022-01-13 17:55:58 -0500

    routes added

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { v4: uuidv4 } = require("uuid");
 var notesDB = require("./db/db.json");
 
 const app = express();
-const PORT = process.env.PORT || 3000 ; 
+const PORT = process.env.PORT || 3001 ; 
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
@@ -17,3 +17,6 @@ app.use(express.static(path.join(__dirname, "./public")));
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "./public/notes.html")));
 app.get("/api/notes", (req,res) => { res.json(notesDB)});
+
+// wildcard route
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
