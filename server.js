26137b5a74abc6862a42684a6eb7c5f436014f25
Ashley <adunderwood96@gmail.com>
2022-01-13 16:49:25 -0500

    making server static

--- server.js
+++ server.js
@@ -8,4 +8,7 @@ var notesDB = require("./db/db.json");
 const app = express();
 const PORT = process.env.PORT || 3000 ; 
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "./public")));
