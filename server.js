cd01bff29379f40b610be205a481e2c69ca5c579
Ashley <adunderwood96@gmail.com>
2022-01-13 16:53:53 -0500

    routes to html/notes

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ var notesDB = require("./db/db.json");
 const app = express();
 const PORT = process.env.PORT || 3000 ; 
 
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname, "./public")));
+
+// routes to get html/notes
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "./public/notes.html")));
+app.get("/api/notes", (req,res) => { res.json(notesDB)});
