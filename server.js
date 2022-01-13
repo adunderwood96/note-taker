655da5fde43ea312a13f2702d2bc9b2b91fb56a5
Ashley <adunderwood96@gmail.com>
2022-01-13 15:38:19 -0500

    adding in const and starting the get apis

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
+// Required 
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+var notesDB = require("./db/db.json");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000 ; 
+
+
