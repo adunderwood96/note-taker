1efa571b59de9a86318b1ea0e6608b3601e78ac3
Ashley <adunderwood96@gmail.com>
2022-01-13 19:15:05 -0500

    working on getting port to work/ npm starterroe

--- server.js
+++ server.js
@@ -1,35 +1,46 @@
 // Required packages
 const express = require("express");
-const fs = require("fs");
 const path = require("path");
-const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+const uuid = require("uuid");
 var notesData = require("./db/db.json");
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static(path.join(__dirname, "./public")));
 
 // routes to get html/notes
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
-app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "./public/notes.html")));
-app.get("/api/notes", (req, res) => { res.json(notesData) });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, ".public/index.html"))
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, ".public/notes.html"))
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notesData)
+});
 
 // wildcard route
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+});
 
 // new note
 app.post("/api/notes", (req, res) => {
-  req.body.id = uuidv4();
+  req.body.id = uuid.v4();
   const newNote = req.body;
 
   notesData.push(newNote);
 
-// note to file
-  fs.writeFileSync("./db/db.json", JSON.stringify(notesData));
+  // note to file
+  fs.writeFileSync(".db/db.json", JSON.stringify(notesData));
   res.json(notesData);
 });
 
@@ -37,9 +48,9 @@ app.post("/api/notes", (req, res) => {
 app.delete("/api/notes/:id", (req, res) => {
   const id = req.params.id;
 
-  notesData = notesData.filter(notes => notes.id != id);
+  notesData = notesData.filter(notes => notes.id !== id);
 
-  fs.writeFileSync("./db/db.json", JSON.stringify(notesData));
+  fs.writeFileSync(".db/db.json", JSON.stringify(notesData));
   res.json(notesData);
 });
 
