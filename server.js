a97f9ffa69d750f02a6117d5b91362301436d28d
Ashley <adunderwood96@gmail.com>
2022-01-18 11:53:26 -0500

    notes look weird/won't save

--- server.js
+++ server.js
@@ -1,23 +1,40 @@
-// Required packages
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+var notesDB = require("./db/db.json");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("/public"));
 
-// routes to note api and html files
-require("./routes/apiRoutes.js")(app);
-require("./routes/htmlRoutes.js")(app);
+app.use(express.static(path.join(__dirname, "./public")));
 
-// wildcard route
-app.get("*", (req, res) => {
-  res.sendFile("/public/index.html")
-});
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "./public/notes.html")));
+app.get("/api/notes", (req, res) => { res.json(notesDB) });
 
-// API Port listener 
-app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
-});
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
+
+app.post("/api/notes", (req, res) => {
+  req.body.id = uuidv4();
+  const newNote = req.body;
+
+  notesDB.push(newNote);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(notesDB));
+  res.json(notesDB);
+})
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+
+  notesDB = notesDB.filter(notes => notes.id != id);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(notesDB));
+  res.json(notesDB);
+})
+
+app.listen(PORT, () => console.log(`App is listening on PORT ${PORT}`));
