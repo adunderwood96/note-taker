957c7dc54c73dd1af95cf125050f952cae06c462
Ashley <adunderwood96@gmail.com>
2022-01-23 20:32:32 -0500

    figured out issue with note not saving

--- server.js
+++ server.js
@@ -1,26 +1,43 @@
 // required packages
 const express = require("express");
 const path = require("path");
-
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+var notesDB = require("./db/db.json");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001 ; 
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "./public")));
 
-// routes
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "./public/notes.html")));
-app.get("/api/notes", (req, res) => { res.json(notesDB)});
+app.get("/api/notes", (req,res) => { res.json(notesDB)});
 
 // wildcard route
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
 
-require("./routes/apiRoutes.js")(app);
-require("./routes/htmlRoutes.js")(app);
+app.post("/api/notes", (req, res) => {
+  req.body.id = uuidv4();
+  const newNote = req.body;
 
-// port listener
-app.listen(PORT, () => console.log(`App is listening on PORT ${PORT}`));
+  notesDB.push(newNote);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(notesDB));
+  res.json(notesDB);
+})
+
+// ability to delete note
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+
+  notesDB = notesDB.filter(notes => notes.id != id);
+
+  fs.writeFileSync("db/db.json", JSON.stringify(notesDB));
+  res.json(notesDB);
+})
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
